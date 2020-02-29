d8d3572b3039758db7285b992b12c31e13f7ef1d
Alexandre Ribeiro do Valle <alexandreribeirodovalle@android-43e0d178af14fb22.localdomain>
2020-02-28 22:11:12 -0300

    este e um novo commit

---
+++ terceiraTentativaComGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace terceiraTentativaComGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("New WOrld");
+
+        }
+    }
+}
